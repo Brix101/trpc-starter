b1fd1b7be0cc1d92f2f64f5d4a7116fdf550108a
Brixter Porras <brixterporras@gmail.com>
2024-12-17 20:34:29 +0800

    feat: added prettier config

---
+++ packages/prettier/index.js
@@ -0,0 +1,28 @@
+
+/** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig  } */
+const config = {
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "4.4.0",
+  overrides: [
+    {
+      files: "*.json.hbs",
+      options: {
+        parser: "json",
+      },
+    },
+    {
+      files: "*.js.hbs",
+      options: {
+        parser: "babel",
+      },
+    },
+  ],
+};
+
+export default config;
